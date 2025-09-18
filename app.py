96ca9d1f4c3be3e87dd6c3023ec6265655b4ebf9
Azbe27 <azbe.petek7@gmail.com>
2025-09-18 09:59:10 +0000

    azbe vaja 2

---
+++ app.py
@@ -0,0 +1,22 @@
+# naredimo strežnik
+from flask import Flask, render_template, request
+import random
+# pip install flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/calcLove")
+def calc():
+    req = request.args
+    ime1 = req.get("ime1")
+    ime2 = req.get("ime2")
+    rnd = random.randint(0,100)
+    if ime1 or ime2 == "azbe":
+        rnd = 100
+    rez = f"{ime1} + {ime2} = {rnd}%"
+    return render_template("index.html" , rezultat = rez)
+
+app.run(debug = True)
